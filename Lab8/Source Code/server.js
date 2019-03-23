d3318636a21a1140d95618c2fac2ac9764f00fc0
sravan <pagadalasravan9393@gmail.com>
2019-03-24 00:50:37 +0530

    commit
    sravan

---
+++ Lab8/Source Code/server.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var app=express();
+var bodyParser= require('body-parser');
+var jwt=require('jsonwebtoken');
+
+var users =[
+    {
+        name:"pagadala",
+        password:"pagadala"
+    },
+    {
+        name:"sravan",
+        password:"sravan"
+    }
+]
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('./'));
+
+app.get('/',(req,res)=>{
+    res.sendFile('index.html');
+});
+
+app.post('/login',(req,res)=>{
+    var message;
+    for(var user of users){
+        if(user.name!=req.body.name){
+            message="Incorect User Name";
+        }else{
+            if(user.password!=req.body.password){
+                message="Incorrect Password";
+                break;
+            }
+            else{
+                var token=jwt.sign(user,"samplesecret");
+                console.log(token);
+                message="Login Account Successful";
+                break;
+            }
+        }
+    }
+    if(token){
+        res.status(200).json({
+            message,
+            token
+        });
+    }
+    else{
+        res.status(403).json({
+            message
+        });
+    }
+});
+
+app.use((req, res, next)=>{
+    // check header or url parameters or post parameters for token
+    console.log(req.body);
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if(token){
+        console.log("token");
+        jwt.verify(token,"samplesecret",(err,decod)=>{
+            if(err){
+                res.status(403).json({
+                    message:"Wrong Token"
+                });
+            }
+            else{
+                console.log("success");
+                req.decoded=decod;
+                next();
+            }
+        });
+    }
+    else{
+        res.status(403).json({
+            message:"No Token"
+        });
+    }
+});
+
+app.post('/getusers',(req,res)=>{
+    var user_list=[];
+    console.log("here");
+    users.forEach((user)=>{
+        user_list.push({"name":user.name});
+    })
+    res.send(JSON.stringify({users:user_list}));
+});
+
+app.listen(3000, function(){
+    console.log('port 3000 is activated');
+});
